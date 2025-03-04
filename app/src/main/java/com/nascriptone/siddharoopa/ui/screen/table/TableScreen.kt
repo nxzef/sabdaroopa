7bc2af64f8c5b2cbe129c3c163c0f7f302ce4fd9
nascriptone <scriptgoat609@gmail.com>
2025-03-04 20:39:26 +0530

    optimize imports

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableScreen.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableScreen.kt
@@ -1,6 +1,5 @@
 package com.nascriptone.siddharoopa.ui.screen.table
 
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
