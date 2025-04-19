ab6a104c83874a87374c196b6555221079577999
nascriptone <scriptgoat609@gmail.com>
2025-04-19 17:59:39 +0530

    bug fixes minor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nascriptone.siddharoopa"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.5.9"
+        versionCode = 11
+        versionName = "1.5.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
