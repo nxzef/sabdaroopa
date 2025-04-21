34a1b9c5c330c098c89c9d86d31e31ce7bc871c7
nascriptone <scriptgoat609@gmail.com>
2025-04-21 10:37:25 +0530

    version update 1.5.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nascriptone.siddharoopa"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.5.11"
+        versionCode = 12
+        versionName = "1.5.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
