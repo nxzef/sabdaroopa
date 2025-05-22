2d829c3aeb73aa3cc1c0eb354e4898286b34d89a
nascriptone <scriptgoat609@gmail.com>
2025-05-22 09:52:03 +0530

    Quiz Updates question screen text logic updates to skt

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizQuestionScreen.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizQuestionScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,9 +28,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.nascriptone.siddharoopa.data.model.uiobj.CaseName
 import com.nascriptone.siddharoopa.data.model.uiobj.FormName
+import com.nascriptone.siddharoopa.data.model.uiobj.Gender
 import com.nascriptone.siddharoopa.ui.component.CurrentState
 import com.nascriptone.siddharoopa.viewmodel.SiddharoopaViewModel
 
@@ -134,8 +137,8 @@ fun QuestionOption(
                     RegexText(each.question, state.data.questionKey)
                     Spacer(Modifier.height(20.dp))
                     state.data.options.forEachIndexed { i, option ->
-                        Text(
-                            option,
+                        OptionText(
+                            text = option,
                             style = MaterialTheme.typography.titleLarge
                         )
                     }
@@ -147,12 +150,18 @@ fun QuestionOption(
                     Row {
                         Column {
                             state.data.options.forEach { option ->
-                                Text(option.key)
+                                OptionText(
+                                    text = option.key,
+                                    style = MaterialTheme.typography.titleLarge
+                                )
                             }
                         }
                         Column {
                             state.data.options.forEach { option ->
-                                Text(option.value)
+                                OptionText(
+                                    text = option.value,
+                                    style = MaterialTheme.typography.titleLarge
+                                )
                             }
                         }
                     }
@@ -160,6 +169,44 @@ fun QuestionOption(
             }
         }
     }
+}
+
+private val caseNameStrings = enumValues<CaseName>().map { it.name }.toSet()
+private val genderNameStrings = enumValues<Gender>().map { it.name }.toSet()
+private val formNameStrings = enumValues<FormName>().map { it.name }.toSet()
+
+
+@Composable
+fun OptionText(
+    text: String,
+    modifier: Modifier = Modifier,
+    style: TextStyle = LocalTextStyle.current
+) {
+    val keyText = text.uppercase()
+    val adjustedText = when (keyText) {
+        in caseNameStrings -> {
+            runCatching { stringResource(enumValueOf<CaseName>(keyText).sktName) }.getOrDefault(
+                keyText
+            )
+        }
+
+        in genderNameStrings -> {
+            runCatching { stringResource(enumValueOf<Gender>(keyText).skt) }.getOrDefault(keyText)
+        }
+
+        in formNameStrings -> {
+            runCatching { stringResource(enumValueOf<FormName>(keyText).sktName) }.getOrDefault(
+                keyText
+            )
+        }
+
+        else -> keyText
+    }
+    Text(
+        adjustedText,
+        style = style,
+        modifier = modifier
+    )
 }
 
 
