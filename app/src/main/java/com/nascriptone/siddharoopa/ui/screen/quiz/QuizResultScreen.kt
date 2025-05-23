56f5d0787c3aec98cd9cade50c7c47defe4ffe4f
nascriptone <scriptgoat609@gmail.com>
2025-05-23 17:20:16 +0530

    Quiz Updates Quiz Result Screen Implementation & UI Logics Changes

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizResultScreen.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizResultScreen.kt
@@ -4,18 +4,33 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.nascriptone.siddharoopa.ui.screen.SiddharoopaRoutes
 import com.nascriptone.siddharoopa.ui.theme.SiddharoopaTheme
 
 @Composable
-fun QuizResultScreen(modifier: Modifier = Modifier) {
+fun QuizResultScreen(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
     Surface {
         Column(
             modifier = modifier
         ) {
             Text("Hello World This is Result Screen")
+            Text("Your result is 100%")
+            TextButton(
+                onClick = {
+                    navHostController.navigate(SiddharoopaRoutes.QuizReview.name)
+                }
+            ) {
+                Text("See Review")
+            }
         }
     }
 }
@@ -25,7 +40,8 @@ fun QuizResultScreen(modifier: Modifier = Modifier) {
 fun QuizResultScreenPreview() {
     SiddharoopaTheme(false) {
         QuizResultScreen(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            navHostController = rememberNavController()
         )
     }
 }
