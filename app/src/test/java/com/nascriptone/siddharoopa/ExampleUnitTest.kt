c352a33778dc708b49dfc2364ee37fa86d61b18f
nascriptone <scriptgoat609@gmail.com>
2025-07-05 20:35:57 +0530

    absolute value equal assertion test

--- app/src/test/java/com/nascriptone/siddharoopa/ExampleUnitTest.kt
+++ app/src/test/java/com/nascriptone/siddharoopa/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.nascriptone.siddharoopa
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -18,5 +20,11 @@ class ExampleUnitTest {
     @Test
     fun subtraction_isCorrect() {
         assertEquals(9, 10 - 1)
+    }
+
+    @Test
+    fun absoluteValueIsCorrect() {
+        val n = -3
+        assertEquals(3, n.absoluteValue)
     }
 }
