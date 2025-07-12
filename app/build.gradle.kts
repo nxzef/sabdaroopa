a530e9c074301a3996173768949f5e0dbcb31b5c
nascriptone <scriptgoat609@gmail.com>
2025-07-12 06:30:54 +0530

    complile sdk from 35 to 36 updates occured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.nascriptone.siddharoopa"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.nascriptone.siddharoopa"
