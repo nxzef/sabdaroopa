6acaae1a653b7f6c31f544dc8172571c73547b36
nxzef <scriptgoat609@gmail.com>
2025-07-25 07:10:24 +0530

    quiz section common components file created for readability & scalability & maintainance

---
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizComponents.kt
@@ -0,0 +1,39 @@
+package com.nascriptone.siddharoopa.ui.screen.quiz
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.nascriptone.siddharoopa.data.model.uiobj.CaseName
+import com.nascriptone.siddharoopa.data.model.uiobj.FormName
+
+@Composable
+fun RegexText(
+    @StringRes template: Int, key: Map<String, String>, modifier: Modifier = Modifier
+) {
+    val mutableKey = key.toMutableMap()
+    val stringRes = stringResource(template)
+    val vibaktiKey = "vibhakti"
+    val vachanaKey = "vachana"
+    if (mutableKey.containsKey(vibaktiKey) || mutableKey.containsKey(vachanaKey)) {
+        val vibaktiValue = key.getValue(vibaktiKey)
+        val vachanaValue = key.getValue(vachanaKey)
+        val vibaktiEnum = CaseName.valueOf(vibaktiValue)
+        val vachanaEnum = FormName.valueOf(vachanaValue)
+        val vibaktiSktName = stringResource(vibaktiEnum.sktName)
+        val vachanaSktName = stringResource(vachanaEnum.sktName)
+        mutableKey[vibaktiKey] = vibaktiSktName
+        mutableKey[vachanaKey] = vachanaSktName
+    }
+    val finalKey = mutableKey.toMap()
+    val text = replacePlaceholders(stringRes, finalKey)
+    Text(text, modifier = modifier, style = MaterialTheme.typography.titleLarge)
+}
+
+private fun replacePlaceholders(template: String, values: Map<String, String>): String {
+    return template.replace(Regex("\\{(\\w+)\\}")) { match ->
+        values[match.groupValues[1]] ?: match.value
+    }
+}
