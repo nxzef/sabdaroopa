e890831522eb9acf6289e3318dfc803a100a5156
nxzef <scriptgoat609@gmail.com>
2025-08-02 12:21:57 +0530

    object logic changes occur

--- app/src/main/java/com/nascriptone/siddharoopa/data/local/QuizResultMessage.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/local/QuizResultMessage.kt
@@ -3,28 +3,30 @@ package com.nascriptone.siddharoopa.data.local
 import com.nascriptone.siddharoopa.R
 
 object QuizResultMessage {
-    val accuracyQuarter = listOf(
-        R.string.quarter_1,
-        R.string.quarter_2,
-        R.string.quarter_3,
-        R.string.quarter_4
-    )
-    val accuracyHalf = listOf(
-        R.string.half_1,
-        R.string.half_2,
-        R.string.half_3,
-        R.string.half_4
-    )
-    val accuracyThreeQuarter = listOf(
-        R.string.three_quarter_1,
-        R.string.three_quarter_2,
-        R.string.three_quarter_3,
-        R.string.three_quarter_4
-    )
-    val accuracyFull = listOf(
-        R.string.full_1,
-        R.string.full_2,
-        R.string.full_3,
-        R.string.full_4
+    val messageList = listOf(
+        listOf(
+            R.string.quarter_1,
+            R.string.quarter_2,
+            R.string.quarter_3,
+            R.string.quarter_4
+        ),
+        listOf(
+            R.string.half_1,
+            R.string.half_2,
+            R.string.half_3,
+            R.string.half_4
+        ),
+        listOf(
+            R.string.three_quarter_1,
+            R.string.three_quarter_2,
+            R.string.three_quarter_3,
+            R.string.three_quarter_4
+        ),
+        listOf(
+            R.string.full_1,
+            R.string.full_2,
+            R.string.full_3,
+            R.string.full_4
+        )
     )
 }
