85ad66de70239b1edd92f6af924b269dfc8bd680
nxzef <scriptgoat609@gmail.com>
2025-08-07 08:43:14 +0530

    resource provider injected to viewModel for string usage

---
+++ app/src/main/java/com/nascriptone/siddharoopa/viewmodel/ResourceProvider.kt
@@ -0,0 +1,12 @@
+package com.nascriptone.siddharoopa.viewmodel
+
+import android.content.Context
+import androidx.annotation.StringRes
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class ResourceProvider @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    fun getString(@StringRes resId: Int): String = context.getString(resId)
+}
