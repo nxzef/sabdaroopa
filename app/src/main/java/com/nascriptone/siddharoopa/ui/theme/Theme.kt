eb1650412040ec63794f5b1fed3f66769b711374
nxzef <scriptgoat609@gmail.com>
2025-08-11 11:16:29 +0530

    file optimization remove redundant comments

--- app/src/main/java/com/nascriptone/siddharoopa/ui/theme/Theme.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/theme/Theme.kt
@@ -3,9 +3,7 @@ package com.nascriptone.siddharoopa.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -13,7 +11,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -260,9 +257,8 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun SiddharoopaTheme(
+fun SabdaroopaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
