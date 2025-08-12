ae15e7eabf3cccbc9654e23bf3aa82b684faa2f8
nxzef <scriptgoat609@gmail.com>
2025-08-12 06:41:32 +0530

    app version name changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 17
-        versionName = "1.7.16"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
