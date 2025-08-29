49cec80f842cd46401f8ed95e6e121a4b97cec41
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:55:06 +0530

    rename file from table screen bar to table bar

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableTopBar.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableTopBar.kt
@@ -2,29 +2,23 @@ package com.nascriptone.siddharoopa.ui.screen.table
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.FavoriteBorder
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.nascriptone.siddharoopa.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TableScreenTopBar(
-    title: String,
     onBackPress: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val titleTail = stringResource(R.string.sabda)
-    val fullTitle = "$title $titleTail"
-    CenterAlignedTopAppBar(
+    TopAppBar(
         title = {
-            Text(fullTitle)
+            Text("Declension")
         },
         navigationIcon = {
             IconButton(onClick = onBackPress) {
