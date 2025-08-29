13b5dad1d804ddb0ecc1e82ae6c6f2a7f040499c
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:47:45 +0530

    app repository logic changes

--- app/src/main/java/com/nascriptone/siddharoopa/data/repository/AppRepository.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/repository/AppRepository.kt
@@ -1,31 +1,16 @@
 package com.nascriptone.siddharoopa.data.repository
 
-import com.nascriptone.siddharoopa.data.local.dao.RestPropDao
-import com.nascriptone.siddharoopa.data.local.dao.GeneralSabdaDao
-import com.nascriptone.siddharoopa.data.local.dao.SpecificSabdaDao
-import com.nascriptone.siddharoopa.data.model.entity.Favorite
-import com.nascriptone.siddharoopa.data.model.entity.RestProp
+import com.nascriptone.siddharoopa.data.local.dao.SabdaDao
 import com.nascriptone.siddharoopa.data.model.entity.Sabda
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 import javax.inject.Inject
 
 class AppRepository @Inject constructor(
-    private val generalSabdaDao: GeneralSabdaDao,
-    private val specificSabdaDao: SpecificSabdaDao,
-    private val restPropDao: RestPropDao
+    private val sabdaDao: SabdaDao
 ) {
+    fun getAllSabda(): Flow<List<Sabda>> = sabdaDao.getAllSabda()
 
+    suspend fun toggleFavorite(id: Int, timeStamp: Long?) = sabdaDao.toggleFavorite(id, timeStamp)
 
-    // General
-    suspend fun getAllGeneralSabda(): List<Sabda> = generalSabdaDao.getAllSabda()
-    // Specific
-    suspend fun getAllSpecificSabda(): List<Sabda> = specificSabdaDao.getAllSabda()
-    // RestProps
-    fun getAllRestProp(): Flow<List<RestProp>> = restPropDao.getAllRestProp()
-
-    suspend fun addRestProp(restProp: RestProp) = restPropDao.addFavoriteToRestProp(restProp)
-
-    suspend fun removeRestProp(favorite: Favorite) = restPropDao.removeFavoriteFromRestProp(favorite)
-
+    suspend fun removeItemsFromFavorite(ids: Set<Int>) = sabdaDao.removeItemsFromFavorite(ids)
 }
