febc48207a6434832e447ebb0163813ff96f7c86
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:52:27 +0530

    file name changes and optimized

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesScreenState.kt
+++
@@ -1,7 +0,0 @@
-package com.nascriptone.siddharoopa.ui.screen.favorites
-
-import com.nascriptone.siddharoopa.data.model.EntireSabda
-
-data class FavoritesScreenState(
-    val sabdaToRemove: EntireSabda? = null,
-)
