c0145c14bce4f5da9c429c4fdde5842e4c4d6003
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:56:11 +0530

    new sabda entity modified

--- app/src/main/java/com/nascriptone/siddharoopa/data/model/entity/Sabda.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/model/entity/Sabda.kt
@@ -1,11 +1,26 @@
 package com.nascriptone.siddharoopa.data.model.entity
 
-interface Sabda {
-    val id: Int
-    val anta: String
-    val gender: String
-    val word: String
-    val sound: String
-    val translit: String
-    val declension: String
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.nascriptone.siddharoopa.data.model.Category
+import com.nascriptone.siddharoopa.data.model.Declension
+import com.nascriptone.siddharoopa.data.model.Gender
+import com.nascriptone.siddharoopa.data.model.Sound
+
+@Entity("sabda")
+data class Sabda(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val anta: String,
+    val word: String,
+    val sound: Sound,
+    val gender: Gender,
+    val meaning: String,
+    val category: Category,
+    val translit: String,
+    @ColumnInfo(name = "is_favorite", defaultValue = "0")
+    val isFavorite: Boolean,
+    @ColumnInfo(name = "favorite_since")
+    val favoriteSince: Long?,
+    val declension: Declension
+)
