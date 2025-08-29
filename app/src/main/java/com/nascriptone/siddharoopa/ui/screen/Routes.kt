8cd2e217934216b72899840a59a5996f5832f8b2
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:56:51 +0530

    routes and navigation classified

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/Routes.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/Routes.kt
@@ -1,20 +1,29 @@
 package com.nascriptone.siddharoopa.ui.screen
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Quiz
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class Navigation(val icon: ImageVector) {
+    Home(Icons.Rounded.Home),
+    Favorites(Icons.Rounded.FavoriteBorder),
+    Quiz(Icons.Rounded.Quiz),
+    Settings(Icons.Rounded.Settings)
+}
+
 enum class Routes {
-    Home,
-    Category,
+    Main,
+    SabdaList,
     Table,
-    SettingsHome,
     FavoritesHome,
     QuizHome,
     QuizQuestion,
     QuizInstruction,
     QuizResult,
-    QuizReview
-}
-
-enum class Navigation {
-    Quiz,
-    Favorites,
-    Settings
+    QuizReview,
+    SettingsHome,
+    Search,
 }
