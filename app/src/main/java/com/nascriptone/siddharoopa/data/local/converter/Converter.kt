a023e857c35efba58d9b29751fdaf46e1d0e859f
nxzef <scriptgoat609@gmail.com>
2025-08-29 17:49:14 +0530

    add database enum and class convertors

--- app/src/main/java/com/nascriptone/siddharoopa/data/local/converter/Converter.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/local/converter/Converter.kt
@@ -1,23 +1,44 @@
 package com.nascriptone.siddharoopa.data.local.converter
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.nascriptone.siddharoopa.data.model.entity.Favorite
+import com.nascriptone.siddharoopa.data.model.Category
+import com.nascriptone.siddharoopa.data.model.Declension
+import com.nascriptone.siddharoopa.data.model.Gender
+import com.nascriptone.siddharoopa.data.model.Sound
+import kotlinx.serialization.json.Json
 
-class Converter {
+class Converters {
+
     companion object {
-        private val gson = Gson()
-    }
-
-    // Favorite
-
-    @TypeConverter
-    fun fromFavorite(favorite: Favorite): String {
-        return gson.toJson(favorite)
+        private val json = Json { encodeDefaults = true; ignoreUnknownKeys = true }
     }
 
     @TypeConverter
-    fun toFavorite(data: String): Favorite {
-        return gson.fromJson(data, Favorite::class.java)
+    fun fromSound(sound: Sound): String = sound.name
+
+    @TypeConverter
+    fun toSound(data: String): Sound = Sound.valueOf(data)
+
+    @TypeConverter
+    fun fromGender(gender: Gender): String = gender.name
+
+    @TypeConverter
+    fun toGender(data: String): Gender = Gender.valueOf(data)
+
+    @TypeConverter
+    fun fromCategory(category: Category): String = category.name
+
+    @TypeConverter
+    fun toCategory(data: String): Category = Category.valueOf(data)
+
+     // Declension
+    @TypeConverter
+    fun fromDeclension(declension: Declension): String {
+        return json.encodeToString(declension)
+    }
+
+    @TypeConverter
+    fun toDeclension(data: String): Declension {
+        return json.decodeFromString(data)
     }
 }
