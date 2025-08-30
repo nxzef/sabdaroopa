2d99b2e8b0ddb5e5d62ebf1de279b423568f44f3
nxzef <scriptgoat609@gmail.com>
2025-08-30 10:23:49 +0530

    category screen view model optmized

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/category/CategoryViewModel.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/category/CategoryViewModel.kt
@@ -2,89 +2,40 @@ package com.nascriptone.siddharoopa.ui.screen.category
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.nascriptone.siddharoopa.data.model.Gender
-import com.nascriptone.siddharoopa.data.model.Sound
-import com.nascriptone.siddharoopa.data.model.entity.Sabda
 import com.nascriptone.siddharoopa.data.repository.AppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryViewModel @Inject constructor(repository: AppRepository) : ViewModel() {
-    private val _sabdaList = repository.getAllSabda().distinctUntilChanged().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = emptyList()
-    )
+class CategoryViewModel @Inject constructor(
+    repository: AppRepository,
+) : ViewModel() {
     private val _filter = MutableStateFlow(Filter())
-    val uiState: StateFlow<CategoryState> = combine(
-        _sabdaList, _filter
-    ) { sabdaList, filter ->
-        val filterState = try {
-            val data = sabdaList.applyFilter(filter)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val uiState: StateFlow<FilterState> = _filter.flatMapLatest { filter ->
+        repository.getFilteredList(filter).map { data ->
             FilterState.Success(data)
-        } catch (e: Exception) {
-            FilterState.Error(e.message ?: "Unknown error occurred")
+        }.onStart {
+            FilterState.Loading
+        }.catch { e ->
+            FilterState.Error(e.message ?: "Unknown error")
         }
-        CategoryState(filter = filter, filterState = filterState)
-    }.distinctUntilChanged().stateIn(
-        scope = viewModelScope,
+    }.stateIn(
+        viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = CategoryState()
+        initialValue = FilterState.Loading
     )
 
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    val uiState: StateFlow<CategoryState> = combine(
-//        _sabdaList, _filter
-//    ) { sabdaList, filter ->
-//        filter to sabdaList
-//    }.flatMapLatest { (filter, sabdaList) ->
-//        flow {
-//            emit(CategoryState(filter = filter, filterState = FilterState.Loading))
-//
-//            try {
-//                val filteredData = withContext(Dispatchers.Default) {
-//                    sabdaList.applyFilter(filter)
-//                }
-//
-//                emit(CategoryState(filter = filter, filterState = FilterState.Success(filteredData)))
-//            } catch (e: Exception) {
-//                emit(CategoryState(filter = filter, filterState = FilterState.Error(e.message ?: "Unknown error occurred")))
-//            }
-//        }
-//    }.stateIn(
-//        scope = viewModelScope,
-//        started = SharingStarted.WhileSubscribed(5_000),
-//        initialValue = CategoryState()
-//    )
-
-
-    fun updateGenderFilter(gender: Gender?) {
-        _filter.update {
-            it.copy(selectedGender = gender)
-        }
-    }
-
-    fun updateSoundFilter(sound: Sound) {
-        _filter.update {
-            it.copy(selectedSound = sound)
-        }
-    }
-
-    fun initializeFilter(filter: Filter) = _filter.update { filter }
-}
-
-private fun List<Sabda>.applyFilter(filter: Filter): List<Sabda> {
-    return this.filter { sabda ->
-        (filter.selectedCategory == null || filter.selectedCategory == sabda.category) &&
-                (filter.selectedSound == null || filter.selectedSound == sabda.sound) &&
-                (filter.selectedGender == null || filter.selectedGender == sabda.gender)
-    }
+    fun updateFilter(filter: Filter) = _filter.update { filter }
 }
