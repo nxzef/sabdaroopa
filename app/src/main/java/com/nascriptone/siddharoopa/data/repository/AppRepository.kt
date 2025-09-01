083c2f097f899962f9353319af4704aed8bb9941
nxzef <scriptgoat609@gmail.com>
2025-09-01 14:26:36 +0530

    pagination enbales on fiiltered list

--- app/src/main/java/com/nascriptone/siddharoopa/data/repository/AppRepository.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/repository/AppRepository.kt
@@ -1,5 +1,7 @@
 package com.nascriptone.siddharoopa.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.nascriptone.siddharoopa.data.local.dao.SabdaDao
 import com.nascriptone.siddharoopa.data.model.entity.Sabda
 import com.nascriptone.siddharoopa.ui.screen.category.Filter
@@ -17,11 +19,19 @@ class AppRepository @Inject constructor(
 
     fun findSabdaById(id: Int): Flow<Sabda?> = sabdaDao.findSabdaById(id)
 
-    fun getFilteredList(filter: Filter): Flow<List<Sabda>> {
-        return sabdaDao.getFilteredList(
-            category = filter.category,
-            sound = filter.sound,
-            gender = filter.gender
+    fun getFilteredList(filter: Filter): Pager<Int, Sabda> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 12,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                sabdaDao.getFilteredList(
+                    category = filter.category,
+                    sound = filter.sound,
+                    gender = filter.gender
+                )
+            }
         )
     }
 }
