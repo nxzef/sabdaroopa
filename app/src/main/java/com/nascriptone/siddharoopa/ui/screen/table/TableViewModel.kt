6b6e972f8791052b452544f3f19f600bbc5e0099
nxzef <scriptgoat609@gmail.com>
2025-09-01 14:25:20 +0530

    Table ViewModel Optmized

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableViewModel.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/table/TableViewModel.kt
@@ -4,14 +4,12 @@ import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nascriptone.siddharoopa.data.model.entity.Sabda
 import com.nascriptone.siddharoopa.data.repository.AppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,14 +24,10 @@ class TableViewModel @Inject constructor(
         "Missing id in SavedStateHandle"
     }
 
-    val tableUIState: StateFlow<FindState> = repository.findSabdaById(id).map { sabda ->
-        sabda?.let { FindState.Found(it) } ?: FindState.NotFound
-    }.catch { e ->
-        emit(FindState.Error(e.message ?: "Database error occurred"))
-    }.distinctUntilChanged().stateIn(
+    val sabda: StateFlow<Sabda?> = repository.findSabdaById(id).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = FindState.Finding
+        initialValue = null
     )
 
     fun toggleFavoriteSabda(id: Int) {
