ccedaf41370311a7639c2530413090ecdbc0266d
nxzef <scriptgoat609@gmail.com>
2025-09-09 16:45:58 +0530

    selection trigger enum rename occured

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
@@ -2,11 +2,11 @@ package com.nascriptone.siddharoopa.ui.screen.favorites
 
 data class FavoritesState(
     val isSelectMode: Boolean = false,
-    val selectionTrigger: SelectionTrigger = SelectionTrigger.NONE,
+    val selectionTrigger: SelectionTrigger = SelectionTrigger.EXPLICIT,
     val selectedIds: Set<Int> = emptySet(),
     val areAllSelected: Boolean = false
 )
 
 enum class SelectionTrigger {
-    NONE, CARD, TOOLBAR
+    EXPLICIT, CARD, TOOLBAR
 }
