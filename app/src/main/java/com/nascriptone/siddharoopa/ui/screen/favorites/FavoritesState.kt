66666290fc00ec54270122137072a6865d3e3e61
nxzef <scriptgoat609@gmail.com>
2025-09-12 06:13:58 +0530

    new prop added to the favorite state to handle the ui changes when it is from quiz

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
@@ -1,13 +1,14 @@
 package com.nascriptone.siddharoopa.ui.screen.favorites
 
 data class FavoritesState(
+    val fromQuiz: Boolean = false,
     val isSelectMode: Boolean = false,
     val selectionTrigger: SelectionTrigger = SelectionTrigger.EXPLICIT,
     val selectedIds: Set<Int> = emptySet(),
-    private val totalIDs: Set<Int> = emptySet()
+    val totalIds: Set<Int> = emptySet()
 ) {
     val areAllSelected: Boolean
-        get() = selectedIds.isNotEmpty() && selectedIds.size == totalIDs.size
+        get() = selectedIds.isNotEmpty() && selectedIds.size == totalIds.size
 }
 
 enum class SelectionTrigger {
