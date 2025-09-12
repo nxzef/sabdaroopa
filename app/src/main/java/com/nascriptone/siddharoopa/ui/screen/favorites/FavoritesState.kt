7fb7ca6da2cfc8857784261461c5f56e905e55dd
nxzef <scriptgoat609@gmail.com>
2025-09-12 17:37:31 +0530

    add AUTO to trigger and rename from selection trigger to trigger

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
@@ -1,9 +1,8 @@
 package com.nascriptone.siddharoopa.ui.screen.favorites
 
 data class FavoritesState(
-    val fromQuiz: Boolean = false,
     val isSelectMode: Boolean = false,
-    val selectionTrigger: SelectionTrigger = SelectionTrigger.EXPLICIT,
+    val trigger: Trigger = Trigger.NONE,
     val selectedIds: Set<Int> = emptySet(),
     val totalIds: Set<Int> = emptySet()
 ) {
@@ -11,6 +10,6 @@ data class FavoritesState(
         get() = selectedIds.isNotEmpty() && selectedIds.size == totalIds.size
 }
 
-enum class SelectionTrigger {
-    EXPLICIT, CARD, TOOLBAR
+enum class Trigger {
+    NONE, AUTO, CARD, TOOLBAR
 }
