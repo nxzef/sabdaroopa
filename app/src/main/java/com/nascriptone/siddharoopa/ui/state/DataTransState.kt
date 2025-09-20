68b96c4429f2bf31abdddb42e8b713ff388ee9dc
nxzef <scriptgoat609@gmail.com>
2025-09-20 17:01:35 +0530

    error prop change to class from object with a message property to show the messages

--- app/src/main/java/com/nascriptone/siddharoopa/ui/state/DataTransState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/state/DataTransState.kt
@@ -1,8 +1,11 @@
 package com.nascriptone.siddharoopa.ui.state
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 sealed interface DataTransState {
     data object None : DataTransState
     data object Loading : DataTransState
     data object Success : DataTransState
-    data object Error : DataTransState
+    data class Error(val message: String) : DataTransState
 }
