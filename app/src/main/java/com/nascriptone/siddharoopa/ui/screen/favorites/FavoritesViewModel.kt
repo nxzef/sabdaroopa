f267f57c72faaa67ee75ecc8583d959856340ebb
nxzef <scriptgoat609@gmail.com>
2025-09-20 17:00:28 +0530

    updateFavoriteSourceData fun logic changes and optimization occured

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesViewModel.kt
@@ -13,15 +13,14 @@ import com.nascriptone.siddharoopa.domain.SourceWithData
 import com.nascriptone.siddharoopa.ui.state.DataTransState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -35,24 +34,12 @@ class FavoritesViewModel @Inject constructor(
         repository.getFavoriteList().flow.cachedIn(viewModelScope)
     private val _uiState = MutableStateFlow(FavoritesState())
     val uiState: StateFlow<FavoritesState> = _uiState.asStateFlow()
-    private val _uiEvent = MutableSharedFlow<DataTransState>()
-    val uiEvent: SharedFlow<DataTransState> = _uiEvent.asSharedFlow()
 
     init {
-        Log.d("MODE", "Triggered in the init block")
-        startFavoritesCollection()
-    }
-
-    fun handleOnClose() {
         viewModelScope.launch {
-            try {
-                if (_uiState.value.trigger == Trigger.AUTO) {
-                    _uiEvent.emit(DataTransState.Success)
-                }
-            } catch (e: Exception) {
-                Log.e("EMIT", "UI Events Emit Failure", e)
-            } finally {
-                toggleSelectionMode()
+            repository.getFavoriteIds().collect { totalIds ->
+                _uiState.update { it.copy(totalIds = totalIds) }
+                handleFavoritesSource()
             }
         }
     }
@@ -85,34 +72,39 @@ class FavoritesViewModel @Inject constructor(
         }
     }
 
-//    private fun updateFavoriteSourceData(data: Set<Int>) {
-//        _uiState.update { it.copy(dataTransState = DataTransState.Loading) }
-//        viewModelScope.launch {
-//            runCatching {
-//                withContext(Dispatchers.IO) {
-//                    delay(1000)
-//                    val words = repository.getWords(data)
-//                    val display = words.joinToString(", ")
-//                    SourceWithData.FromFavorites(
-//                        data = data,
-//                        display = display
-//                    )
-//                }
-//            }.onSuccess { sourceWithData ->
-//                sharedDataDomain.updateSourceWithData(sourceWithData)
-//                _uiState.update { it.copy(dataTransState = DataTransState.Success) }
-//            }.onFailure {
-//                _uiState.update { it.copy(dataTransState = DataTransState.Error) }
-//            }
-//        }
-//    }
-
-    private fun startFavoritesCollection() {
+    fun updateSourceWithData(empty: Boolean = false) {
         viewModelScope.launch {
-            Log.d("MODE", "Triggered in the init block on startFavoritesCollection")
-            repository.getFavoriteIds().collect { totalIds ->
-                _uiState.update { it.copy(totalIds = totalIds) }
-                handleFavoritesSource()
+            val data = if (empty) emptySet() else _uiState.value.selectedIds
+            updateFavoriteSourceData(data)
+        }
+    }
+
+    fun dismissError() = _uiState.update { it.copy(dataTransState = DataTransState.None) }
+
+    private suspend fun updateFavoriteSourceData(data: Set<Int>) {
+        _uiState.update { it.copy(dataTransState = DataTransState.Loading) }
+        runCatching {
+            withContext(Dispatchers.IO) {
+                delay(1200)
+                val words = repository.getWords(data)
+                val display = words.joinToString(", ")
+                SourceWithData.FromFavorites(
+                    data = data,
+                    display = display
+                )
+            }
+        }.onSuccess { sourceWithData ->
+            sharedDataDomain.updateSourceWithData(sourceWithData)
+            _uiState.update { it.copy(dataTransState = DataTransState.Success) }
+            toggleSelectionMode()
+        }.onFailure { throwable ->
+            Log.d("DATA_TRANS", "Data Transfer Error", throwable)
+            _uiState.update {
+                it.copy(
+                    dataTransState = DataTransState.Error(
+                        message = throwable.message ?: "Unknown Error"
+                    )
+                )
             }
         }
     }
@@ -120,7 +112,6 @@ class FavoritesViewModel @Inject constructor(
     private fun handleFavoritesSource() {
         val sourceData = sharedDataDomain.sourceWithData.value
         if (sourceData is SourceWithData.FromFavorites && _uiState.value.trigger == Trigger.NONE) {
-            Log.d("MODE", "Triggered in the init block on handleFavoritesSource")
             toggleSelectionMode(Trigger.AUTO)
             _uiState.update { it.copy(selectedIds = sourceData.data) }
         }
