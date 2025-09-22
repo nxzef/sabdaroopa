3e9551ae0b7a3dae11c80204ad141744880b4721
nxzef <scriptgoat609@gmail.com>
2025-09-22 16:08:56 +0530

    add quiz viewModel scope to the navigation level

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizInstructionScreen.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/quiz/QuizInstructionScreen.kt
@@ -17,19 +17,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.nascriptone.siddharoopa.R
-import com.nascriptone.siddharoopa.ui.theme.SabdaroopaTheme
 
 @Composable
 fun QuizInstructionScreen(
     onBackPress: () -> Unit,
+    quizViewModel: QuizViewModel,
     modifier: Modifier = Modifier
 ) {
+    val uiState by quizViewModel.uiState.collectAsStateWithLifecycle()
     Surface {
         Column(
             modifier = modifier
@@ -188,13 +190,5 @@ fun SectionView(
         )
         Spacer(Modifier.height(8.dp))
         content()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun QuizInstructionScreenPreview() {
-    SabdaroopaTheme(true) {
-        QuizInstructionScreen(onBackPress = {})
     }
 }
