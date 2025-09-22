642e91ddd6c96ee25ded13452f58cd52a9993b65
nxzef <scriptgoat609@gmail.com>
2025-09-22 16:06:11 +0530

    remove data transfer state property from favorite state

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
@@ -1,13 +1,10 @@
 package com.nascriptone.siddharoopa.ui.screen.favorites
 
-import com.nascriptone.siddharoopa.ui.state.DataTransState
-
 data class FavoritesState(
     val isSelectMode: Boolean = false,
     val trigger: Trigger = Trigger.NONE,
     val selectedIds: Set<Int> = emptySet(),
     val totalIds: Set<Int> = emptySet(),
-    val dataTransState: DataTransState = DataTransState.None
 ) {
     val areAllSelected: Boolean
         get() = selectedIds.isNotEmpty() && selectedIds.size == totalIds.size
