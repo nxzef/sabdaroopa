99b5610f0bff67fee21b25f2dc0e757454398895
nxzef <scriptgoat609@gmail.com>
2025-09-27 16:27:39 +0530

    favorite state changes

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesState.kt
@@ -5,11 +5,18 @@ data class FavoritesState(
     val trigger: Trigger = Trigger.NONE,
     val selectedIds: Set<Int> = emptySet(),
     val totalIds: Set<Int> = emptySet(),
+    val transferState: TransferState? = null
 ) {
     val areAllSelected: Boolean
         get() = selectedIds.isNotEmpty() && selectedIds.size == totalIds.size
 }
 
 enum class Trigger {
-    NONE, AUTO, CARD, TOOLBAR
+    NONE, INIT, CARD, TOOLBAR
+}
+
+sealed class TransferState {
+    data object Loading : TransferState()
+    data object Success : TransferState()
+    data class Error(val message: String) : TransferState()
 }
