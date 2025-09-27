3b9ff92382b3d1670443d8b20c759361242522e1
nxzef <scriptgoat609@gmail.com>
2025-09-27 16:29:00 +0530

    favorite viewModel work in progress

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesTopBar.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesTopBar.kt
@@ -10,11 +10,14 @@ import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Close
@@ -23,18 +26,21 @@ import androidx.compose.material.icons.rounded.Deselect
 import androidx.compose.material.icons.rounded.Mode
 import androidx.compose.material.icons.rounded.Quiz
 import androidx.compose.material.icons.rounded.SelectAll
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -46,6 +52,7 @@ import com.nascriptone.siddharoopa.ui.component.CustomDialog
 import com.nascriptone.siddharoopa.ui.component.CustomDialogDescription
 import com.nascriptone.siddharoopa.ui.component.CustomDialogHead
 import com.nascriptone.siddharoopa.ui.component.CustomToolTip
+import com.nascriptone.siddharoopa.ui.component.DialogLayout
 import com.nascriptone.siddharoopa.ui.screen.Navigation
 import com.nascriptone.siddharoopa.utils.extensions.sharedViewModelOrNull
 
@@ -59,70 +66,60 @@ fun FavoritesTopBar(
         navHostController.sharedViewModelOrNull(Navigation.Favorites.name)
     if (favoritesViewModel == null) return
     val uiState by favoritesViewModel.uiState.collectAsStateWithLifecycle()
-
-    val onTakeQuizClick = remember(navHostController) {
-        {
-            navHostController.navigate(Navigation.Quiz.name) {
-                popUpTo(Navigation.Favorites.name) {
-                    saveState = true
-                    inclusive = true
-                }
-                restoreState = true
-            }
-        }
-    }
-
     AnimatedContent(
         targetState = uiState.isSelectMode, transitionSpec = {
             fadeIn() + scaleIn(initialScale = 0.8f) togetherWith fadeOut() + scaleOut(targetScale = 1.2f)
         }) { isSelectMode ->
-        if (isSelectMode) {
-            FavoriteActionTopBar(
-                onClose = {
-                    if (uiState.trigger == Trigger.AUTO) onTakeQuizClick()
-                    else favoritesViewModel.toggleSelectionMode()
-                },
-                onTakeQuizClick = onTakeQuizClick,
-                favoritesViewModel = favoritesViewModel
-            )
-        } else {
-            TopAppBar(
-                title = {
-                    Text("Favorites")
-                }, navigationIcon = {
-                    CustomToolTip("Back") {
-                        IconButton(onClick = navHostController::navigateUp) {
-                            Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
+        if (isSelectMode) FavoriteActionTopBar(favoritesViewModel = favoritesViewModel)
+        else TopAppBar(
+            title = {
+                Text("Favorites")
+            }, navigationIcon = {
+                CustomToolTip("Back") {
+                    IconButton(onClick = navHostController::navigateUp) {
+                        Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
+                    }
+                }
+            }, actions = {
+                if (uiState.totalIds.isNotEmpty()) {
+                    CustomToolTip("Select") {
+                        IconButton(onClick = {
+                            favoritesViewModel.toggleSelectionMode(trigger = Trigger.TOOLBAR)
+                        }) {
+                            Icon(Icons.Rounded.Mode, null)
                         }
                     }
-                }, actions = {
-                    if (uiState.totalIds.isNotEmpty()) {
-                        CustomToolTip("Select") {
-                            IconButton(onClick = {
-                                favoritesViewModel.toggleSelectionMode(trigger = Trigger.TOOLBAR)
-                            }) {
-                                Icon(Icons.Rounded.Mode, null)
-                            }
-                        }
+                }
+            }, modifier = modifier
+        )
+    }
+    uiState.transferState?.let { transferState ->
+        TransferDialog(
+            transferState = transferState,
+            onDismissRequest = favoritesViewModel::transferDialogDismiss,
+            onRetryClick = favoritesViewModel::onTakeQuizClick,
+            onSuccess = {
+                navHostController.navigate(Navigation.Quiz.name) {
+                    popUpTo(Navigation.Favorites.name) {
+                        inclusive = true
                     }
-                }, modifier = modifier
-            )
-        }
+                }
+                favoritesViewModel.toggleSelectionMode()
+            }
+        )
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteActionTopBar(
-    onClose: () -> Unit,
-    onTakeQuizClick: () -> Unit,
     favoritesViewModel: FavoritesViewModel,
     modifier: Modifier = Modifier,
 ) {
     val uiState by favoritesViewModel.uiState.collectAsStateWithLifecycle()
     var deleteDialogVisible by rememberSaveable { mutableStateOf(false) }
 
-    BackHandler(onBack = onClose)
+    BackHandler(onBack = favoritesViewModel::toggleSelectionMode)
 
     Surface {
         Row(
@@ -140,7 +137,7 @@ fun FavoriteActionTopBar(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 CustomToolTip("Close") {
-                    IconButton(onClick = onClose) {
+                    IconButton(onClick = favoritesViewModel::toggleSelectionMode) {
                         val imageVector = Icons.Rounded.Close
                         Icon(imageVector, imageVector.name)
                     }
@@ -159,14 +156,16 @@ fun FavoriteActionTopBar(
                     exit = fadeOut() + scaleOut(targetScale = 1.2f)
                 ) {
                     CustomToolTip("Quiz") {
-                        IconButton(onClick = onTakeQuizClick) {
+                        IconButton(onClick = favoritesViewModel::onTakeQuizClick) {
                             val imageVector = Icons.Rounded.Quiz
                             Icon(imageVector, imageVector.name)
                         }
                     }
+                    AnimatedVisibility(true) { }
+                    AnimatedVisibility(false) { }
                 }
                 AnimatedVisibility(
-                    visible = isNotEmpty && uiState.trigger != Trigger.AUTO,
+                    visible = isNotEmpty && uiState.trigger != Trigger.INIT,
                     enter = fadeIn() + scaleIn(initialScale = 0.8f),
                     exit = fadeOut() + scaleOut(targetScale = 1.2f)
                 ) {
@@ -203,6 +202,57 @@ fun FavoriteActionTopBar(
             },
             onDismissRequest = { deleteDialogVisible = false },
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TransferDialog(
+    transferState: TransferState,
+    onDismissRequest: () -> Unit,
+    onRetryClick: () -> Unit,
+    onSuccess: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    if (transferState is TransferState.Success) LaunchedEffect(Unit) { onSuccess() }
+    else BasicAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier
+    ) {
+        DialogLayout {
+            when (transferState) {
+                is TransferState.Loading -> Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(20.dp)
+                ) {
+                    CircularProgressIndicator()
+                    Spacer(Modifier.width(16.dp))
+                    Text("Updating...")
+                }
+
+                is TransferState.Error -> Column(modifier = Modifier.padding(16.dp)) {
+                    CustomDialogHead("Error")
+                    Spacer(Modifier.height(4.dp))
+                    CustomDialogDescription(transferState.message)
+                    Spacer(Modifier.height(12.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        TextButton(onClick = onDismissRequest) {
+                            Text("Cancel")
+                        }
+                        Spacer(Modifier.width(8.dp))
+                        TextButton(onClick = onRetryClick) {
+                            Text("Retry")
+                        }
+                    }
+                }
+
+                else -> Unit
+            }
+        }
     }
 }
 
