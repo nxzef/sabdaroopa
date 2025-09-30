27eff5ef539fd2e953b36ab09fa5313fa79d3ba3
nxzef <scriptgoat609@gmail.com>
2025-09-30 21:31:59 +0530

    added the update button and it's behaviour

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesTopBar.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/favorites/FavoritesTopBar.kt
@@ -3,6 +3,9 @@ package com.nascriptone.siddharoopa.ui.screen.favorites
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
@@ -26,7 +29,9 @@ import androidx.compose.material.icons.rounded.Deselect
 import androidx.compose.material.icons.rounded.Mode
 import androidx.compose.material.icons.rounded.Quiz
 import androidx.compose.material.icons.rounded.SelectAll
+import androidx.compose.material.icons.rounded.Update
 import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -45,6 +50,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
@@ -73,24 +79,24 @@ fun FavoritesTopBar(
         if (isSelectMode) FavoriteActionTopBar(favoritesViewModel = favoritesViewModel)
         else TopAppBar(
             title = {
-                Text("Favorites")
-            }, navigationIcon = {
-                CustomToolTip("Back") {
-                    IconButton(onClick = navHostController::navigateUp) {
-                        Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
+            Text("Favorites")
+        }, navigationIcon = {
+            CustomToolTip("Back") {
+                IconButton(onClick = navHostController::navigateUp) {
+                    Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
+                }
+            }
+        }, actions = {
+            if (uiState.totalIds.isNotEmpty()) {
+                CustomToolTip("Select") {
+                    IconButton(onClick = {
+                        favoritesViewModel.toggleSelectionMode(trigger = Trigger.TOOLBAR)
+                    }) {
+                        Icon(Icons.Rounded.Mode, null)
                     }
                 }
-            }, actions = {
-                if (uiState.totalIds.isNotEmpty()) {
-                    CustomToolTip("Select") {
-                        IconButton(onClick = {
-                            favoritesViewModel.toggleSelectionMode(trigger = Trigger.TOOLBAR)
-                        }) {
-                            Icon(Icons.Rounded.Mode, null)
-                        }
-                    }
-                }
-            }, modifier = modifier
+            }
+        }, modifier = modifier
         )
     }
     uiState.transferState?.let { transferState ->
@@ -103,10 +109,10 @@ fun FavoritesTopBar(
                     popUpTo(Navigation.Favorites.name) {
                         inclusive = true
                     }
+                    launchSingleTop = true
                 }
                 favoritesViewModel.toggleSelectionMode()
-            }
-        )
+            })
     }
 }
 
@@ -117,6 +123,7 @@ fun FavoriteActionTopBar(
     modifier: Modifier = Modifier,
 ) {
     val uiState by favoritesViewModel.uiState.collectAsStateWithLifecycle()
+    val hasSelectionChanged by favoritesViewModel.hasSelectionChanged.collectAsStateWithLifecycle()
     var deleteDialogVisible by rememberSaveable { mutableStateOf(false) }
 
     BackHandler(onBack = favoritesViewModel::toggleSelectionMode)
@@ -142,32 +149,16 @@ fun FavoriteActionTopBar(
                         Icon(imageVector, imageVector.name)
                     }
                 }
-                Text(
-                    "${uiState.selectedIds.size}", style = MaterialTheme.typography.titleLarge
-                )
+                Text("${uiState.selectedIds.size}", style = MaterialTheme.typography.titleLarge)
             }
             Row(
                 verticalAlignment = Alignment.CenterVertically, modifier = Modifier
             ) {
                 val isNotEmpty = uiState.selectedIds.isNotEmpty()
                 AnimatedVisibility(
-                    visible = isNotEmpty,
-                    enter = fadeIn() + scaleIn(initialScale = 0.8f),
-                    exit = fadeOut() + scaleOut(targetScale = 1.2f)
-                ) {
-                    CustomToolTip("Quiz") {
-                        IconButton(onClick = favoritesViewModel::onTakeQuizClick) {
-                            val imageVector = Icons.Rounded.Quiz
-                            Icon(imageVector, imageVector.name)
-                        }
-                    }
-                    AnimatedVisibility(true) { }
-                    AnimatedVisibility(false) { }
-                }
-                AnimatedVisibility(
                     visible = isNotEmpty && uiState.trigger != Trigger.INIT,
                     enter = fadeIn() + scaleIn(initialScale = 0.8f),
-                    exit = fadeOut() + scaleOut(targetScale = 1.2f)
+                    exit = fadeOut() + scaleOut(targetScale = 0.8f)
                 ) {
                     CustomToolTip("Delete") {
                         IconButton(onClick = { deleteDialogVisible = true }) {
@@ -176,16 +167,41 @@ fun FavoriteActionTopBar(
                         }
                     }
                 }
+                if (uiState.trigger == Trigger.INIT) CustomToolTip("Update") {
+                    val scale by animateFloatAsState(
+                        targetValue = if (hasSelectionChanged) 1f else 0.9f,
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        ),
+                    )
+                    Button(
+                        onClick = favoritesViewModel::onTakeQuizClick,
+                        enabled = hasSelectionChanged,
+                        modifier = Modifier.scale(scale)
+                    ) {
+                        Icon(Icons.Rounded.Update, null)
+                        Spacer(Modifier.width(8.dp))
+                        Text("Update")
+                    }
+                } else CustomToolTip("Quiz") {
+                    IconButton(
+                        onClick = favoritesViewModel::onTakeQuizClick, enabled = isNotEmpty
+                    ) {
+                        val imageVector = Icons.Rounded.Quiz
+                        Icon(imageVector, imageVector.name)
+                    }
+                }
                 AnimatedContent(
                     targetState = uiState.areAllSelected
                 ) { state ->
                     if (state) CustomToolTip("Deselect All") {
-                        IconButton(onClick = favoritesViewModel::toggleFavoriteSelectAll) {
+                        IconButton(onClick = favoritesViewModel::toggleSelectAllFavorites) {
                             val imageVector = Icons.Rounded.Deselect
                             Icon(imageVector, imageVector.name)
                         }
                     } else CustomToolTip("Select All") {
-                        IconButton(onClick = favoritesViewModel::toggleFavoriteSelectAll) {
+                        IconButton(onClick = favoritesViewModel::toggleSelectAllFavorites) {
                             val imageVector = Icons.Rounded.SelectAll
                             Icon(imageVector, imageVector.name)
                         }
@@ -216,8 +232,7 @@ fun TransferDialog(
 ) {
     if (transferState is TransferState.Success) LaunchedEffect(Unit) { onSuccess() }
     else BasicAlertDialog(
-        onDismissRequest = onDismissRequest,
-        modifier = modifier
+        onDismissRequest = onDismissRequest, modifier = modifier
     ) {
         DialogLayout {
             when (transferState) {
