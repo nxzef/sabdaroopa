102ed3adf836a2d3ad327bbee7aaf249ec06a161
nxzef <scriptgoat609@gmail.com>
2025-10-18 20:05:12 +0530

    add params to string resources

--- app/src/main/java/com/nascriptone/siddharoopa/ui/screen/settings/AppPreferencesState.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/ui/screen/settings/AppPreferencesState.kt
@@ -7,7 +7,7 @@ data class AppPreferencesState(
     val currentTheme: Theme = Theme.SYSTEM
 )
 
-enum class Theme(@StringRes val uiName: Int) {
+enum class Theme(@param:StringRes val uiName: Int) {
     SYSTEM(R.string.system_default),
     LIGHT(R.string.light),
     DARK(R.string.dark)
