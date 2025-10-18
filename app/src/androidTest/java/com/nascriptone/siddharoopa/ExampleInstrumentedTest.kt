3b3146abb0515da9dafd0aca7ea0525fc27647e9
nxzef <scriptgoat609@gmail.com>
2025-10-18 19:46:43 +0530

    file optimized

--- app/src/androidTest/java/com/nascriptone/siddharoopa/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nascriptone/siddharoopa/ExampleInstrumentedTest.kt
@@ -1,16 +1,10 @@
 package com.nascriptone.siddharoopa
 
-import android.annotation.SuppressLint
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.unit.dp
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
