fff39d9fa5eceaeb2f5c5144f401e86466674f14
nxzef <scriptgoat609@gmail.com>
2025-10-18 20:09:44 +0530

    remove gson & make libs.plugins.kotlin.serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
     kotlin("plugin.serialization") version "2.0.21"
