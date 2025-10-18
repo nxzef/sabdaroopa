9ac73108e9949fd73d42168655dd6722d6d9cc57
nxzef <scriptgoat609@gmail.com>
2025-10-18 20:11:33 +0530

    optimize files and remove gson and add json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
-    kotlin("plugin.serialization") version "2.0.21"
-    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -35,13 +33,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-        freeCompilerArgs = listOf("-XXLanguage:+PropertyParamAnnotationDefaultTargetMode")
     }
     buildFeatures {
         compose = true
@@ -90,9 +81,6 @@ dependencies {
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
-
-    // Gson
-    implementation(libs.gson)
 
     // Animation
     implementation(libs.androidx.animation)
