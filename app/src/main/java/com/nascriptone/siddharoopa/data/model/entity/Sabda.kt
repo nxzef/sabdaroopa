c72cbd25ab497ce5cba675a6cb9f9f8f06d3203a
nxzef <scriptgoat609@gmail.com>
2025-10-19 08:30:50 +0530

    add indices to the sabda entity

--- app/src/main/java/com/nascriptone/siddharoopa/data/model/entity/Sabda.kt
+++ app/src/main/java/com/nascriptone/siddharoopa/data/model/entity/Sabda.kt
@@ -2,13 +2,21 @@ package com.nascriptone.siddharoopa.data.model.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.nascriptone.siddharoopa.data.model.Category
 import com.nascriptone.siddharoopa.data.model.Declension
 import com.nascriptone.siddharoopa.data.model.Gender
 import com.nascriptone.siddharoopa.data.model.Sound
 
-@Entity(tableName = "sabda")
+@Entity(
+    tableName = "sabda",
+    indices = [
+        Index(value = ["category"], name = "idx_sabda_category"),
+        Index(value = ["gender"], name = "idx_sabda_gender"),
+        Index(value = ["sound"], name = "idx_sabda_sound")
+    ]
+)
 data class Sabda(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val anta: String,
