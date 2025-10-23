27e671f7cf4deece857aa8f7b9951659a9eb8b43
nxzef <scriptgoat609@gmail.com>
2025-10-23 10:16:31 +0530

    refactor: remove DataStoreProvider

--- app/src/main/java/com/nascriptone/siddharoopa/di/DataStoreProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.nascriptone.siddharoopa.di
-
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-
-private const val USER_PREFERENCE_NAME = "user_preferences"
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(USER_PREFERENCE_NAME)
